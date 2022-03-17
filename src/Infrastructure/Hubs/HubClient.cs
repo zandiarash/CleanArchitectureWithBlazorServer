d5e9cd10d454421a1db630df0b993857adddce9e
neozhu <neo.js.cn@gmail.com>
2022-03-17 21:02:48 +0800

    Update HubClient.cs

--- src/Infrastructure/Hubs/HubClient.cs
+++ src/Infrastructure/Hubs/HubClient.cs
@@ -104,11 +104,11 @@ public class HubClient : IAsyncDisposable
         await StopAsync();
     }
 
-    public event EventHandler<string> LoggedIn;
-    public event EventHandler<string> LoggedOut;
-    public event EventHandler<string> NotificationReceived;
-    public event MessageReceivedEventHandler MessageReceived;
-    public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
+    public event EventHandler<string>? LoggedIn;
+    public event EventHandler<string>? LoggedOut;
+    public event EventHandler<string>? NotificationReceived;
+    public event MessageReceivedEventHandler? MessageReceived;
+    public delegate Task MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
 
     public class MessageReceivedEventArgs : EventArgs
     {
