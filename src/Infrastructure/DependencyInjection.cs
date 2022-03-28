727d47552e250e2db8e7b5e53cd9991761ad3c04
neozhu <neo.js.cn@gmail.com>
2022-03-28 12:31:33 +0800

    add _loading

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -19,7 +19,7 @@ public static class DependencyInjection
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseInMemoryDatabase("BlazorDashboardDb")
-               );
+                );
         }
         else
         {
@@ -27,7 +27,7 @@ public static class DependencyInjection
                 options.UseSqlServer(
                     configuration.GetConnectionString("DefaultConnection"),
                     b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
-               );
+                ,ServiceLifetime.Transient, ServiceLifetime.Transient);
             services.AddDatabaseDeveloperPageExceptionFilter();
         }
 
