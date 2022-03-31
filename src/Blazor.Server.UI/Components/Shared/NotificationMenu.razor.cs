e2d6bcad69c8c788a069fcc1b4f6195d59293db0
neozhu <neo.js.cn@gmail.com>
2022-03-31 19:03:13 +0800

    Update NotificationMenu.razor.cs

--- src/Blazor.Server.UI/Components/Shared/NotificationMenu.razor.cs
+++ src/Blazor.Server.UI/Components/Shared/NotificationMenu.razor.cs
@@ -1,9 +1,5 @@
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using MudBlazor;
-using MudBlazor.Utilities;
-using Blazor.Server.UI.Models.Notification;
-using Blazor.Server.UI.Services;
 using Blazor.Server.UI.Services.Notifications;
 
 namespace Blazor.Server.UI.Components.Shared;
@@ -24,7 +20,7 @@ public partial class NotificationMenu : MudComponentBase
     {
         if (firstRender)
         {
-           (NotificationService as InMemoryNotificationService).Preload();
+           (NotificationService as InMemoryNotificationService)?.Preload();
             _newNotificationsAvailable = await NotificationService.AreNewNotificationsAvailable();
             _messages = await NotificationService.GetNotifications();
             StateHasChanged();
