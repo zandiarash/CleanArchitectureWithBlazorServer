b3c8f9a2950799aad06c7d646e3e0efc0c2be62e
neozhu <new163@163.com>
2023-02-15 14:27:17 +0800

    Create CommonExceptionHandler.cs

---
+++ src/Application/Common/ExceptionHandler/CommonExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Blazor.Application.Constants;
+
+namespace CleanArchitecture.Blazor.Application.Common.ExceptionHandler;
+public class GenericExceptionHandler<TRequest, TResponse, TException> : IRequestExceptionHandler<TRequest, TResponse, TException> 
+    where TRequest:notnull 
+    where TException : Exception
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public GenericExceptionHandler(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+    public Task Handle(TRequest request, TException exception, RequestExceptionHandlerState<TResponse> state, CancellationToken cancellationToken)
+    {
+        var requestName = nameof(request);
+        _logger.LogError(exception, "Application Request: Exception for Request {Name} {@Request}", requestName, request);
+        return Task.CompletedTask;
+    }
+}
